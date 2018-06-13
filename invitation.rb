8d57ad9f4a1d50250c730fd5fd64ac2632b5d595
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-13 16:56:32 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,12 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+# outsputs a welcome message
 puts "Hi, you've been invited to a party! What is your name?"
-
+# captures the guest name
 guest_name = gets.chomp
+# asking for which party they have been invited to
+puts "Can I know which party you have invited to?"
+# getting information for party_name
+party_name = gets.chomp
 
